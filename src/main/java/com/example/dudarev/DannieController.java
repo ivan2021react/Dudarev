68c91055975719a355d9c8c2ceb5b89225cfc551
ivan2021react <ivan.inbev@mail.ru>
2021-12-13 15:57:57 +0800

    Commit #9

--- src/main/java/com/example/dudarev/DannieController.java
+++ src/main/java/com/example/dudarev/DannieController.java
@@ -1,4 +1,35 @@
 package com.example.dudarev;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class DannieController {
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button singUpButton;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField familiy;
+
+    @FXML
+    private TextField strana;
+
+    @FXML
+    private CheckBox men;
+
+    @FXML
+    private CheckBox woman;
+
 }
--- src/main/java/com/example/dudarev/HelloController.java
+++ src/main/java/com/example/dudarev/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.dudarev;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
 
@@ -25,11 +30,34 @@ public class HelloController {
     private Button vhodButton;
 
     @FXML
-    private Button loginButton;
+    private Button loginSignButton;
 
     @FXML
     void initialize() {
+        loginSignButton.setOnAction(event -> {
+            loginSignButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("view-dannie.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
 
 
     }
 }
+
+
+
+
+
+
+
+
+
